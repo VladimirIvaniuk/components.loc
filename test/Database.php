47669f9d01d654c8c359118cd4d3d6865f2be5bf
VladimirIvaniuk <ivanyuk85vladimir@gmail.com>
2021-06-01 00:08:45 +0300

    add func get and delete

---
+++ test/Database.php
@@ -0,0 +1,84 @@
+<?php
+
+class Database
+{
+    private static $instance = null;
+    private $pdo, $query, $error = false, $results, $count;
+
+    private function __construct()
+    {
+        try {
+            $this->pdo = new PDO('mysql:host=localhost;dbname=marlin_part_2;', "root", "root");
+        } catch (PDOException $exception) {
+            die($exception->getMessage());
+        }
+    }
+
+    public static function GetInstance()
+    {
+        if (!isset(self::$instance)) {
+            self::$instance = new Database();
+        }
+        return self::$instance;
+    }
+
+    public function query($sql, $params = [])
+    {
+
+        $this->error = false;
+        $this->query = $this->pdo->prepare($sql);
+        if (count($params)) {
+            $i = 1;
+            foreach ($params as $param) {
+                $this->query->bindValue($i, $param);
+                $i++;
+            }
+        }
+        if (!$this->query->execute()) {
+            $this->error = true;
+        } else {
+            $this->results = $this->query->fetchAll(PDO::FETCH_OBJ);
+            $this->count = $this->query->rowCount();
+        }
+        return $this;
+    }
+
+    public function get($table, $where = [])
+    {
+        return $this->action('SELECT *', $table, $where);
+    }
+    public function delete($table, $where=[]){
+        return $this->action('DELETE', $table, $where);
+    }
+    public function action($action, $table, $where = []){
+        if (count($where) === 3) {
+            $operators = ['=', '>', '<', '>=', '<='];
+
+            $field = $where[0];
+            $operator = $where[1];
+            $value = $where[2];
+            if (in_array($operator, $operators)) {
+                $sql = "{$action} FROM {$table} where {$field} {$operator} ?";
+                if (!$this->query($sql, [$value])->error()) {
+                    return $this;
+                }
+            }
+        }
+        return false;
+    }
+
+    public function result()
+    {
+        return $this->results;
+    }
+
+    public function error()
+    {
+        return $this->error;
+    }
+
+    public function count()
+    {
+        return $this->count;
+    }
+}
---
+++ test/index.php
@@ -0,0 +1,14 @@
+<?php
+require_once "Database.php";
+
+//$users=Database::GetInstance()->query('select * from comments where name IN (?, ?)', ['Petya', 'Vasya']);
+$users=Database::GetInstance()->get('comments', ['name','=','Petya']);
+//Database::GetInstance()->delete('comments', ['name','=','Vasya']);
+if($users->error()){
+    echo "Error";
+}
+else{
+    foreach ($users->result() as $user){
+        echo $user->name. "</br>";
+    }
+}
